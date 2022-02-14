e0a5a62670649f8038644604448ebb3a48bd3978
Llikeyoulqm <86854333+Llikeyoulqm@users.noreply.github.com>
2022-02-14 23:28:53 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -275,8 +275,8 @@ getData() {
             #resolve=`curl -sL https://hijk.art/hostip.php?d=${DOMAIN}`
             #res=`echo -n ${resolve} | grep ${IP}`
             if [[ -z "${res}" ]]; then
-                #colorEcho ${BLUE}  "${DOMAIN} 解析结果：${resolve}"
-                #colorEcho ${RED}  " 域名未解析到当前服务器IP(${IP})!"
+                colorEcho ${BLUE}  "${DOMAIN} 解析结果：${resolve}"
+                colorEcho ${RED}  " 域名未解析到当前服务器IP(${IP})!"
                 #exit 1
             fi
         fi
